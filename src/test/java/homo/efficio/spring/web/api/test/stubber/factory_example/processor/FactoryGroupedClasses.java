028e7cfdf855359a2652e70a15b97c24181669f3
HomoEfficio <homo.efficio@gmail.com>
2016-11-23 15:28:45 +0900

    factory예제 - FactoryGroupClasses 추가

---
+++ src/test/java/homo/efficio/spring/web/api/test/stubber/factory_example/processor/FactoryGroupedClasses.java
@@ -0,0 +1,34 @@
+package homo.efficio.spring.web.api.test.stubber.factory_example.processor;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author omwomw@sk.com
+ *         created on 2016. 11. 22.
+ */
+public class FactoryGroupedClasses {
+
+    private String qualifiedClassName;
+
+    private Map<String, FactoryAnnotatedClass> itemsMap =
+            new LinkedHashMap<>();
+
+    public FactoryGroupedClasses(String qualifiedClassName) {
+        this.qualifiedClassName = qualifiedClassName;
+    }
+
+    public void add(FactoryAnnotatedClass toInsert) {
+
+        FactoryAnnotatedClass existing = itemsMap.get(toInsert.getId());
+        if (existing != null) {
+            throw new IllegalArgumentException("Class already exists: " + existing);
+        }
+
+        itemsMap.put(toInsert.getId(), toInsert);
+    }
+
+//    public void generateCode(Elements elementUtils, Filer filer) throws IOException {
+//	...
+//    }
+}

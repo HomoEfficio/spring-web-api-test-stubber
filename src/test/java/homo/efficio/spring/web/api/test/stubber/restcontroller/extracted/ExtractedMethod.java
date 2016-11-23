332bf452be0703a9930eb7e69cab25e385844f2e
HomoEfficio <homo.efficio@gmail.com>
2016-11-24 00:50:59 +0900

    Extracted 류 클래스 추가

---
+++ src/test/java/homo/efficio/spring/web/api/test/stubber/restcontroller/extracted/ExtractedMethod.java
@@ -0,0 +1,8 @@
+package homo.efficio.spring.web.api.test.stubber.restcontroller.extracted;
+
+/**
+ * @author homo.efficio@gmail.com
+ *         created on 2016-11-23.
+ */
+public class ExtractedMethod {
+}
---
+++ src/test/java/homo/efficio/spring/web/api/test/stubber/restcontroller/extracted/ExtractedRestController.java
@@ -0,0 +1,54 @@
+package homo.efficio.spring.web.api.test.stubber.restcontroller.extracted;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.MirroredTypeException;
+import java.util.Optional;
+
+/**
+ * @author homo.efficio@gmail.com
+ *         created on 2016-11-23.
+ */
+public class ExtractedRestController {
+
+    private TypeElement annotatedClassElement;
+    private String qualifiedClassName;
+    private String simpleClassName;
+    private String annotationValue;
+    private String[] reqMappedURL;
+
+    public ExtractedRestController(TypeElement annotatedClassElement) {
+        this.annotatedClassElement = annotatedClassElement;
+
+        RestController restControllerAnnotation = annotatedClassElement.getAnnotation(RestController.class);
+        this.annotationValue = restControllerAnnotation.value();
+
+        Optional<RequestMapping> optReqMappingAnnotation = Optional.ofNullable(annotatedClassElement.getAnnotation(RequestMapping.class));
+        optReqMappingAnnotation.ifPresent(o -> this.reqMappedURL = o.value());
+
+//        if (StringUtils.isEmpty(annotationValue)) {
+//            throw new IllegalArgumentException(
+//                    String.format("id() in @%s for class %s is null or empty! that's not allowed",
+//                            RestController.class.getSimpleName(), annotatedClassElement.getQualifiedName().toString()));
+//        }
+
+        if (StringUtils.isNoneEmpty(reqMappedURL)) {
+            System.out.println("  reqMappedURL: [" + String.join(", ", reqMappedURL) + "]");
+        }
+
+        try {
+            Class<?> clazz = restControllerAnnotation.getClass();
+            qualifiedClassName = clazz.getCanonicalName();
+            simpleClassName = clazz.getSimpleName();
+        } catch (MirroredTypeException mte) {
+            DeclaredType classTypeMirror = (DeclaredType) mte.getTypeMirror();
+            TypeElement classTypeElement = (TypeElement) classTypeMirror.asElement();
+            qualifiedClassName = classTypeElement.getQualifiedName().toString();
+            simpleClassName = classTypeElement.getSimpleName().toString();
+        }
+    }
+}

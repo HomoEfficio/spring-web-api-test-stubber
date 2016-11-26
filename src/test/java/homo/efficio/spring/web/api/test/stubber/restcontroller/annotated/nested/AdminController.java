6d8189432d66cba18fd9a90be110d9e63d7ced0c
HomoEfficio <homo.efficio@gmail.com>
2016-11-26 18:08:23 +0900

    테스트 용 샘플 컨트롤러 폴더 이동 복원

---
+++ src/test/java/homo/efficio/spring/web/api/test/stubber/restcontroller/annotated/nested/AdminController.java
@@ -0,0 +1,18 @@
+package homo.efficio.spring.web.api.test.stubber.restcontroller.annotated.nested;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author homo.efficio@gmail.com
+ *         created on 2016-11-23.
+ */
+@RestController
+public class AdminController {
+
+    @RequestMapping("/abc")
+    public ResponseEntity save() {
+        return null;
+    }
+}
